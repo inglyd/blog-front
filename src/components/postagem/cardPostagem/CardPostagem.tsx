42ee7c2ceeeb743a6dceef761f0553ae65b0de47
inglyd <inglydalves@outlook.com>
2025-10-30 21:28:01 -0300

    feat: lógica dos componentes CardPostagem e ListaPostagens

--- src/components/postagem/cardPostagem/CardPostagem.tsx
+++ src/components/postagem/cardPostagem/CardPostagem.tsx
@@ -1,6 +1,12 @@
 import { Link } from "react-router-dom";
+import type Postagem from "../../../models/Postagem";
 
-function CardPostagem() {
+interface CardPostagensProps {
+    postagem: Postagem
+}
+
+/* Atribuição por Desestruturação, acessa as props do obj tema diretamente, sem precisar referenciar o objeto completo */
+function CardPostagem({ postagem }: CardPostagensProps) {
   return (
     <div
       className="border-slate-900 border 
@@ -9,19 +15,25 @@ function CardPostagem() {
       <div>
         <div className="flex w-full bg-indigo-400 py-2 px-4 items-center gap-4">
           <img
-            src="https://i.imgur.com/pK6vSCy.png"
+            src={postagem.usuario?.foto}
             className="h-12 rounded-full"
-            alt=""
+            alt={postagem.usuario?.nome}
           />
           <h3 className="text-lg font-bold text-center uppercase">
-            Nome do Usuário
+            {/* Se existir postagem.usuario, mostro o nome;
+        se ainda não existir, mostro nada e sigo em frente. 
+        'operador de encadeamento opcional (?.)'*/}
+            {postagem.usuario?.nome}
           </h3>
         </div>
         <div className="p-4 ">
-          <h4 className="text-lg font-semibold uppercase">Titulo</h4>
-          <p>texto</p>
-          <p>Tema: </p>
-          <p>Data: </p>
+          <h4 className="text-lg font-semibold uppercase">{postagem.titulo}</h4>
+          <p>{postagem.texto}</p>
+          <p>Tema: {postagem.tema?.descricao}</p>
+          <p>Data: {new Intl.DateTimeFormat("pt-BR", {
+            dateStyle: 'full',
+            timeStyle: 'medium',
+          }).format(new Date(postagem.data))}</p>
         </div>
       </div>
       <div className="flex">
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,5 @@
+import ListaPostagens from "../../components/postagem/listaPostagens/ListaPostagens";
+
 function Home() {
   return (
     <>
@@ -5,7 +7,9 @@ function Home() {
         <div className="container grid grid-cols-2 text-white">
           <div className="flex flex-col gap-4 items-center justify-center py-4">
             <h2 className="text-5xl font-bold">Seja Bem Vinde!</h2>
-            <p className="text-xl">Expresse aqui seus pensamentos e opniões</p>
+            <p className="text-xl">
+              Expresse aqui seus pensamentos e opniões
+            </p>
 
             <div className="flex justify-around gap-4">
               <div
@@ -26,6 +30,7 @@ function Home() {
           </div>
         </div>
       </div>
+      <ListaPostagens />
     </>
   );
 }
